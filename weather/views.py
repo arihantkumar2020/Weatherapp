d50ab1390fa47b63396884e6ba521c42c4dd4401
arihantkumar2020 <arihant.kumar@hotmail.com>
2020-03-17 22:45:18 +0530

    Fourth commit

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,5 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
 
@@ -9,6 +9,8 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=c871a83df1450729638413a1dc4d7067'
 
     err_msg = ''
+    message = ''
+    message_class = ''
 
     if request.method == 'POST':
         form = CityForm(request.POST)
@@ -18,9 +20,20 @@ def index(request):
             existing_city_count = City.objects.filter(name=new_city).count()
 
             if existing_city_count == 0 :
-                form.save()
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist in the world.'
             else:
                 err_msg = 'City does not exist'
+
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully'
+            message_class = 'is-success'
 
     form = CityForm()
 
@@ -43,7 +56,14 @@ def index(request):
 
     context = {
         'weather_data' : weather_data,
-        'form' : form
+        'form' : form,
+        'message' : message,
+        'message_class' : message_class,
     }
 
     return render(request, 'weather/weather.html', context)
+
+
+def delete_city(requests, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
